d6d0133a7592e8498b31acbf728108e64df05919
tangquochung <19521583@gm.uit.edu.vn>
2022-10-14 09:34:33 +0700

    First React web

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Body from "./components/Body"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/about" exact component={About} />
           <Route path="/contact" exact component={Contact} />
         </Routes>
+        <Body />
         <Footer />
       </Router>
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
       <div className="socialMedia">
         <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
       </div>
-      <p> &copy; 2021 pedrotechpizza.com</p>
+      <p> &copy; 2022 Made by Quoc Hung</p>
     </div>
   );
 }
